b1096eea3bde1515e1835ac44ae7c68b3a1fa5cc
varunvasudevan94 <varunvasudevan11@gmail.com>
2021-04-10 14:02:04 +0530

    Add notifications registration to App

--- remembrall-client/App.tsx
+++ remembrall-client/App.tsx
@@ -1,14 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+import { registerForPushNotificationsAsync, Notifications } from './Notifications';
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then((token: string | undefined ) => token ? setExpoPushToken(token) : null); 
+
+    // This listener is fired whenever a notification is received while the app is foregrounded
+    // @ts-ignore
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      setNotification(notification as any);
+    });
+
+    return () => {
+      // @ts-ignore
+      Notifications.removeNotificationSubscription(notificationListener.current);
+      // @ts-ignore
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
 
   if (!isLoadingComplete) {
     return null;
--- remembrall-client/Notifications.ts
+++ remembrall-client/Notifications.ts
@@ -42,3 +42,4 @@ export async function registerForPushNotificationsAsync() {
     return token;
   }
   
+  export {Notifications};
